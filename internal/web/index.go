b10198b95ae7d0b77fd3cc2cca7258e2203b1b0f
lanthora <lanthora@outlook.com>
2022-05-26 10:49:21 +0800

    主页内容以源码形式嵌入

--- internal/web/index.go
+++ internal/web/index.go
@@ -2,10 +2,18 @@
 package web
 
 import (
+	_ "embed"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	//go:embed index.html
+	indexContent []byte
 )
 
 func index(context *gin.Context) {
 	// 这里是 Web 模块在后端项目唯一需要引用的前端资源
-	context.File("/usr/share/hackernel/web/index.html")
+	context.Data(http.StatusOK, "text/html; charset=utf-8", indexContent)
 }
