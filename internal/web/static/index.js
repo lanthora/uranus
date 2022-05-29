6932266fb19f093035722228804ecd7cc0385493
lanthora <lanthora@outlook.com>
2022-05-29 08:44:18 +0800

    添加主页跳转登录页

---
+++ internal/web/static/index.js
@@ -0,0 +1,37 @@
+const { AppBar, Toolbar, Typography } = window['MaterialUI'];
+
+function UserInfoBar() {
+        return (
+                <AppBar position="static">
+                        <Toolbar variant="dense">
+                                <Typography variant="h6" color="inherit" component="div" id="alias">
+                                        张三
+                                </Typography>
+                        </Toolbar>
+                </AppBar>
+        )
+}
+
+function Index() {
+        return (
+                <span>
+                        <UserInfoBar />
+                </span>
+        )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+axios.post('/user/current', {})
+        .then(function (response) {
+                root.render(<Index />);
+                console.log(response);
+        })
+        .catch(function (error) {
+                if (error.response && error.response.status === 401) {
+                        window.location.replace("login");
+                }
+        });
+
+
