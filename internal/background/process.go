ab70dc694f419f9b91be8213543fab05ef9f550f
lanthora <lanthora@outlook.com>
2022-08-20 04:21:32 +0000

    服务异常退出自动重启

--- internal/background/process.go
+++ internal/background/process.go
@@ -263,6 +263,7 @@ func (w *ProcessWorker) run() {
 	defer w.wg.Done()
 	w.dog = watchdog.New(10*time.Second, func() {
 		logrus.Error("osinfo::report timeout")
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 	})
 	defer w.dog.Stop()
 	for w.running {
