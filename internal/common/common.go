8e96e64de237eae88940bd7e4eb04f769349c16f
lanthora <lanthora@outlook.com>
2022-10-10 06:08:47 +0000

    修复内存数据库无效

--- internal/common/common.go
+++ internal/common/common.go
@@ -12,18 +12,17 @@ import (
 func GetDataSourceNameFromConfig(config *viper.Viper) (dataSourceName string) {
 	dbFile := strings.TrimPrefix(config.GetString("db"), "file:")
 
-	if dbFile == ":memory:" {
-		logrus.Info("Currently using an in-memory database, data will be lost when the process exits")
-		dataSourceName = "file:" + dbFile
-		return
-	}
-
 	if strings.ContainsAny(dbFile, "?=&") {
 		logrus.Fatal("Path contains invalid characters")
 		return
 	}
 
-	os.MkdirAll(filepath.Dir(dbFile), os.ModeDir)
+	if dbFile == ":memory:" {
+		logrus.Info("Currently using an in-memory database, data will be lost when the process exits")
+	} else {
+		os.MkdirAll(filepath.Dir(dbFile), os.ModeDir)
+	}
+
 	dbOptions := "?cache=shared&mode=rwc&_journal_mode=WAL"
 	dataSourceName = "file:" + dbFile + dbOptions
 	return
