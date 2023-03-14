3d64d9c9ae2fde06d3704aec48b2ddb31df2091a
lanthora <lanthora@outlook.com>
2023-03-14 20:16:40 +0800

    补充文件和网络通知

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -13,6 +13,8 @@ import (
 
 	"github.com/gen2brain/beeep"
 	"github.com/lanthora/uranus/internal/web/process"
+	"github.com/lanthora/uranus/pkg/file"
+	"github.com/lanthora/uranus/pkg/net"
 	"github.com/sirupsen/logrus"
 )
 
@@ -24,16 +26,20 @@ type NotifyWorker struct {
 	server             string
 	client             *http.Client
 	ProcessEventOffset int64
+	FileEventOffset    int64
+	NetEventOffset     int64
 }
 
 const notifyNumberMax = 10
 
-func NewWorker(server, username, password string, processEventOffset int64) *NotifyWorker {
+func NewWorker(server, username, password string, processEventOffset, fileEventOffset, netEventOffset int64) *NotifyWorker {
 	w := NotifyWorker{
 		server:             server,
 		username:           username,
 		password:           password,
 		ProcessEventOffset: processEventOffset,
+		FileEventOffset:    fileEventOffset,
+		NetEventOffset:     netEventOffset,
 	}
 	return &w
 }
@@ -74,83 +80,6 @@ func (w *NotifyWorker) Start() (err error) {
 	go w.run()
 	return
 }
-func (w *NotifyWorker) notify(title, message string) {
-	err := beeep.Notify(title, message, "")
-	if err != nil {
-		logrus.Error(err)
-		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-		return
-	}
-	time.Sleep(500 * time.Millisecond)
-}
-
-func (w *NotifyWorker) run() {
-	defer w.wg.Done()
-	for w.running {
-		body, err := json.Marshal(map[string]int64{"offset": w.ProcessEventOffset, "limit": 5000})
-		if err != nil {
-			logrus.Error(err)
-			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-			return
-		}
-
-		url := w.server + "/process/listEvents"
-		contentType := "application/json"
-		resp, err := w.client.Post(url, contentType, bytes.NewBuffer(body))
-		if err != nil {
-			logrus.Error(err)
-			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-			return
-		}
-
-		bytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			logrus.Error(err)
-			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-			return
-		}
-
-		doc := struct {
-			Status  int             `json:"status"`
-			Message string          `json:"message"`
-			Data    []process.Event `json:"data"`
-		}{}
-		err = json.Unmarshal(bytes, &doc)
-		if err != nil {
-			logrus.Error(err)
-			syscall.Kill(syscall.Getpid(), syscall.SIGINT)
-			return
-		}
-		if doc.Data == nil {
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		for idx, event := range doc.Data {
-			w.ProcessEventOffset = event.ID
-			title := fmt.Sprintf("进程防护事件 (ID: %d)", event.ID)
-			message := event.Argv
-			w.notify(title, message)
-
-			if idx > notifyNumberMax {
-				break
-			}
-		}
-
-		if w.ProcessEventOffset != doc.Data[len(doc.Data)-1].ID {
-			title := "进程防护事件"
-			message := "已忽略积压的通知,请通过网页查看"
-			w.notify(title, message)
-
-			event := doc.Data[len(doc.Data)-1]
-			w.ProcessEventOffset = event.ID
-			title = fmt.Sprintf("进程防护事件 (ID: %d)", w.ProcessEventOffset)
-			message = event.Argv
-			w.notify(title, message)
-			break
-		}
-	}
-}
 
 func (w *NotifyWorker) Stop() {
 	w.running = false
@@ -170,4 +99,219 @@ func (w *NotifyWorker) Stop() {
 		return
 	}
 	logrus.Infof("Logout: %s", bytes)
+}
+
+func (w *NotifyWorker) run() {
+	defer w.wg.Done()
+	for w.running {
+		w.updateProcessNotify()
+		w.updateFileNotify()
+		w.updateNetNotify()
+		time.Sleep(time.Second)
+	}
+}
+
+func (w *NotifyWorker) notify(title, message string) {
+	err := beeep.Notify(title, message, "")
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+	time.Sleep(500 * time.Millisecond)
+}
+
+func (w *NotifyWorker) updateProcessNotify() {
+	body, err := json.Marshal(map[string]int64{"offset": w.ProcessEventOffset, "limit": 5000})
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	url := w.server + "/process/listEvents"
+	contentType := "application/json"
+	resp, err := w.client.Post(url, contentType, bytes.NewBuffer(body))
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	doc := struct {
+		Status  int             `json:"status"`
+		Message string          `json:"message"`
+		Data    []process.Event `json:"data"`
+	}{}
+
+	err = json.Unmarshal(bytes, &doc)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	if doc.Data == nil {
+		return
+	}
+
+	for idx, event := range doc.Data {
+		w.ProcessEventOffset = event.ID
+		title := fmt.Sprintf("进程防护事件 (ID: %d)", event.ID)
+		message := event.Argv
+		w.notify(title, message)
+
+		if idx > notifyNumberMax {
+			break
+		}
+	}
+
+	if w.ProcessEventOffset != doc.Data[len(doc.Data)-1].ID {
+		title := "进程防护事件"
+		message := "已忽略积压的通知,请通过网页查看"
+		w.notify(title, message)
+
+		event := doc.Data[len(doc.Data)-1]
+		w.ProcessEventOffset = event.ID
+		title = fmt.Sprintf("进程防护事件 (ID: %d)", w.ProcessEventOffset)
+		message = event.Argv
+		w.notify(title, message)
+	}
+}
+
+func (w *NotifyWorker) updateFileNotify() {
+	body, err := json.Marshal(map[string]int64{"offset": w.FileEventOffset, "limit": 5000})
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	url := w.server + "/file/listEvents"
+	contentType := "application/json"
+	resp, err := w.client.Post(url, contentType, bytes.NewBuffer(body))
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	doc := struct {
+		Status  int          `json:"status"`
+		Message string       `json:"message"`
+		Data    []file.Event `json:"data"`
+	}{}
+
+	err = json.Unmarshal(bytes, &doc)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	if doc.Data == nil {
+		return
+	}
+
+	for idx, event := range doc.Data {
+		w.FileEventOffset = event.ID
+		title := fmt.Sprintf("文件防护事件 (ID: %d)", event.ID)
+		message := event.Path
+		w.notify(title, message)
+
+		if idx > notifyNumberMax {
+			break
+		}
+	}
+
+	if w.FileEventOffset != doc.Data[len(doc.Data)-1].ID {
+		title := "文件防护事件"
+		message := "已忽略积压的通知,请通过网页查看"
+		w.notify(title, message)
+
+		event := doc.Data[len(doc.Data)-1]
+		w.FileEventOffset = event.ID
+		title = fmt.Sprintf("文件防护事件 (ID: %d)", w.FileEventOffset)
+		message = event.Path
+		w.notify(title, message)
+	}
+}
+
+func (w *NotifyWorker) updateNetNotify() {
+	body, err := json.Marshal(map[string]int64{"offset": w.NetEventOffset, "limit": 5000})
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	url := w.server + "/net/listEvents"
+	contentType := "application/json"
+	resp, err := w.client.Post(url, contentType, bytes.NewBuffer(body))
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	doc := struct {
+		Status  int         `json:"status"`
+		Message string      `json:"message"`
+		Data    []net.Event `json:"data"`
+	}{}
+
+	err = json.Unmarshal(bytes, &doc)
+	if err != nil {
+		logrus.Error(err)
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+		return
+	}
+
+	if doc.Data == nil {
+		return
+	}
+
+	for idx, event := range doc.Data {
+		w.NetEventOffset = event.ID
+		title := fmt.Sprintf("网络防护事件 (ID: %d)", event.ID)
+		message := fmt.Sprintf("%s:%d => %s:%d", event.SrcAddr, event.SrcPort, event.DstAddr, event.DstPort)
+		w.notify(title, message)
+
+		if idx > notifyNumberMax {
+			break
+		}
+	}
+
+	if w.NetEventOffset != doc.Data[len(doc.Data)-1].ID {
+		title := "网络防护事件"
+		message := "已忽略积压的通知,请通过网页查看"
+		w.notify(title, message)
+
+		event := doc.Data[len(doc.Data)-1]
+		w.NetEventOffset = event.ID
+		title = fmt.Sprintf("网络防护事件 (ID: %d)", w.NetEventOffset)
+		message = fmt.Sprintf("%s:%d => %s:%d", event.SrcAddr, event.SrcPort, event.DstAddr, event.DstPort)
+		w.notify(title, message)
+	}
 }
